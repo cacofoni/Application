369dc9ca7243156e958565cbc6afa2e0a2665cdb
George P. Burdell <cacofoni2016@gmail.com>
2016-02-03 14:18:42 -0800

    Disable Admin Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   post 'feedbacks' => 'feedbacks#create', :as => :feedback
   get 'feedbacks/new' => 'feedbacks#new', :as => :new_feedback
   get '/comments/:id', to: 'comments#show', :as => :comment
   
   root to: 'rooms#show'
   
+  
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
